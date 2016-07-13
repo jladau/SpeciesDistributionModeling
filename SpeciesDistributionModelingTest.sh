c4af25833ddcf58a45e7d80681b7db526ddc8cd9
jladau <jladau@gmail.com>
2016-07-12 22:39:43 -0700

    2016-07-12

--- SpeciesDistributionModelingTest.sh
+++ SpeciesDistributionModelingTest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #loading local variables
+#TODO: the sDir variable needs to be set to the directory containing this script.
 sDir=$HOME/Documents/Research/Java/Distribution/SpeciesDistributionModeling
 rm -f -r $sDir/test/output
 mkdir $sDir/test/output
+cd $sDir
 
 #outputting list of acceptable variables from MESS output (MESS output modified with sed command for this example so there are usable variables)
 #sed 's/false$/true/g' $sDir/test/output/TestOutput.MESS.csv | grep "true$" | cut -d\, -f1 | sed 's/:/\,/g' > $sDir/test/output/RasterList.MESS.csv
@@ -11,33 +13,40 @@ mkdir $sDir/test/output
 #grep "true$" $sDir/test/output/TestOutput.MESS.csv | cut -d\, -f1 | sed 's/:/\,/g' > $sDir/test/output/RasterList.MESS.csv
 
 #running model selection
+echo '------------------------------------------------------'
 echo 'Running model selection...'
 echo ''
-java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelSelector.ModelSelectorLauncher --sBIOMPath=$sDir/test/TestData.biom --sRasterListPath=$sDir/test/RasterList.csv --sResponseVarsListPath=$sDir/test/ResponseVariables.csv --sOutputPath=$sDir/test/output/TestOutput.ModelSelection.csv
-echo ''
+java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelSelector.ModelSelectorLauncher --sBIOMPath=$sDir/test/TestData.biom --sRasterListPath=$sDir/test/RasterList.csv --sResponseVarsListPath=$sDir/test/ResponseVariables.csv --sOutputPath=$sDir/test/output/TestOutput.ModelSelection.csv --bPrintData=true
+#echo '------------------------------------------------------'
 echo ''
 
 #projecting maps
+echo '------------------------------------------------------'
+echo ''
 echo 'Projecting maps...'
 echo ''
-java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelProjector.ModelProjectorLauncher --sOutputDir=$sDir/test/output --sSelectedModelsPath=$sDir/test/output/TestOutput.ModelSelection.csv --sResponseVarsListPath=$sDir/test/ResponseVariables.csv --sBIOMPath=$sDir/test/TestData.biom
+java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelProjector.ModelProjectorLauncher --sOutputPath=$sDir/test/output/otu1[logit].nc --sSelectedModelsPath=$sDir/test/output/TestOutput.ModelSelection.csv --sBIOMPath=$sDir/test/TestData.biom --iInputLine=1
+echo ''
+java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelProjector.ModelProjectorLauncher --sOutputPath=$sDir/test/output/otu2[logit].nc --sSelectedModelsPath=$sDir/test/output/TestOutput.ModelSelection.csv --sBIOMPath=$sDir/test/TestData.biom --iInputLine=2
+echo ''
+java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.ModelProjector.ModelProjectorLauncher --sOutputPath=$sDir/test/output/Richness[log10].nc --sSelectedModelsPath=$sDir/test/output/TestOutput.ModelSelection.csv --sBIOMPath=$sDir/test/TestData.biom --iInputLine=3
+#echo '------------------------------------------------------'
+echo ''
 
 #running MESS analysis
-cd $sDir
+echo '------------------------------------------------------'
 echo ''
 echo 'Running MESS analysis...'
 echo ''
-java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.MESS.MESSLauncher --sBIOMPath=$sDir/test/TestData.biom --sOutputPath=$sDir/test/output/TestOutput.MESS.nc --sRasterListPath=$sDir/test/RasterList.csv --bOutputTable=true --bOutputMap=true
-echo ''
+java -cp bin/SpeciesDistributionModeling.jar edu.ucsf.MESS.MESSLauncher --sOutputPath=$sDir/test/output/TestOutput.MESS.nc --sSelectedModelsPath=$sDir/test/output/TestOutput.ModelSelection.csv --sBIOMPath=$sDir/test/TestData.biom --bOutputTable=true --bOutputMap=true --iInputLine=1
+#echo '------------------------------------------------------'
 echo ''
 
-echo ''
-echo ''
-echo '*************************************'
+echo '******************************************************'
+echo 'Validating output...'
 iMESSDiff=`diff test/output/TestOutput.MESS.csv test/output.correct/TestOutput.MESS.csv | wc -l`
 iMESSMapDiff=`cmp test/output/TestOutput.MESS.nc test/output.correct/TestOutput.MESS.nc | wc -l`
-iMESSRasterList=`diff test/output/RasterList.MESS.csv test/output.correct/RasterList.MESS.csv | wc -l`
-iMESS=$(($iMESSDiff+$iMESSMapDiff+$iMESSRasterList))
+iMESS=$(($iMESSDiff+$iMESSMapDiff))
 iSelectionDiff=`diff test/output/TestOutput.ModelSelection.csv test/output.correct/TestOutput.ModelSelection.csv | wc -l`
 iSelectionDataDiff=`diff test/output/TestOutput.ModelSelection.data.csv test/output.correct/TestOutput.ModelSelection.data.csv | wc -l`
 iSelection=$(($iSelectionDiff+$iSelectionDataDiff))
@@ -70,5 +79,5 @@ then
 else
 	echo 'ERROR: One or more tests failed.'
 fi
-echo '*************************************'
+echo '******************************************************'
 
